1db6a76f74a40e252d8afeda547188d3e675aa07
Jorge Salas <j.saulff@gmail.com>
2020-11-25 16:54:12 -0600

    Pad number done

--- app/src/main/java/com/sabina/calculator/MainActivity.kt
+++ app/src/main/java/com/sabina/calculator/MainActivity.kt
@@ -2,10 +2,54 @@ package com.sabina.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var _valueOne: Float = 0.0f
+    var _valuteTwo: Float = 0.0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val display: TextView = findViewById(R.id.display)
+
+        // Number
+        one.setOnClickListener {
+            display.text = display.text.toString() + "1"
+        }
+
+        two.setOnClickListener {
+            display.text = display.text.toString() + "2"
+        }
+
+        three.setOnClickListener {
+            display.text = display.text.toString() + "3"
+        }
+
+        four.setOnClickListener {
+            display.text = display.text.toString() + "4"
+        }
+
+        five.setOnClickListener {
+            display.text = display.text.toString() + "5"
+        }
+
+        six.setOnClickListener {
+            display.text = display.text.toString() + "6"
+        }
+
+        seven.setOnClickListener {
+            display.text = display.text.toString() + "7"
+        }
+
+        eight.setOnClickListener {
+            display.text = display.text.toString() + "8"
+        }
+
+        nine.setOnClickListener {
+            display.text = display.text.toString() + "9"
+        }
     }
 }
